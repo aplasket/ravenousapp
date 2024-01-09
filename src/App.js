7a906139af3d613a1b95b0a5ba32ffe79d6d614a
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-09 11:46:08 -0500

    update app with business list and search bar and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import styles from './App.css';
+import React from "react";
+
+import BusinessList from "./BusinessList";
+import SearchBar from './SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Codecademy!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
     </div>
   );
 }
